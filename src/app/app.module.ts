8d84094f7d6965b44c5d422243db915038d8e896
pujanban21 <pujan_567@hotmail.com>
2017-08-29 10:58:12 -0500

    adding a server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import {ServerComponent} from './server/server.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent   //adding additional component
   ],
-  imports: [
+  imports: [  //simply add other module to this module
     BrowserModule,
     FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //for the main component
 })
 export class AppModule { }
