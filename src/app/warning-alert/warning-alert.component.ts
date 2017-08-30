57d324800ffa926d44af5029f18b41e15a36ee56
pujanban21 <pujan_567@hotmail.com>
2017-08-30 09:44:40 -0500

    minor changes to the components

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector:'app-warning-alert',
-  templateUrl:'./warning-alert.component.html',
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
   styleUrls: ['./warning-alert.component.css']
 })
 
