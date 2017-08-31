118804919dbf0ca527d895c4709cc3429cea578d
pujanban21 <pujan_567@hotmail.com>
2017-08-30 22:39:38 -0500

    custom event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,7 +11,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  serverCreationStatus ='no server was created';
+  serverCreationStatus ='No server was created';
+  serverName = '';
   constructor() {
     setTimeout(()=> {
       this.allowNewServer = true;
@@ -23,5 +24,10 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: Event){
+  //  console.log(event);
+  this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
